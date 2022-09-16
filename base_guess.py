09a0be9b9b760caf4fb0a952af3944b9c3e56086
pedrolopesc <pedro.araca@gmail.com>
2022-09-15 23:32:03 -0300

    feat: ETL words dataframe

--- base_guess.py
+++ base_guess.py
@@ -22,3 +22,19 @@ def has_wy(dfin: pd.DataFrame) -> pd.DataFrame:
 
 def max_letters(li_vowels: list[int]) -> int:
     return sum(li_vowels)
+
+def create_dataset(dfin: pd.DataFrame) -> pd.DataFrame:
+    df = dfin.copy()
+    df['len'] = df['palavras'].apply(lambda x: len(x))
+    df['acento'] = df['palavras'].apply(lambda x: x != unidecode(x))
+    df['sigla'] = df['palavras'].apply(lambda x: x == x.upper())
+    df['nome_capital'] = df['palavras'].apply(lambda x: x == x.lower())
+    dfout = df[(df.len == 5) & (~df.acento) & (~df.sigla) & (df.nome_capital)].copy()
+    dfout = dfout.pipe(has_wy)
+    dfout['vec_palav'] = dfout['palavras'].apply(lambda x: list(x))
+    dfout['vec_aeiou'] = dfout['palavras'].apply(count_vowels)
+    dfout['vec_conso'] = dfout['palavras'].apply(count_consonants)
+
+    dfout['count_vowels'] = dfout['vec_aeiou'].apply(max_letters)
+    dfout['count_cons'] = dfout['vec_conso'].apply(max_letters)
+    return dfout[['palavras', 'vec_palav', 'vec_aeiou', 'count_vowels', 'vec_conso', 'count_cons']]
